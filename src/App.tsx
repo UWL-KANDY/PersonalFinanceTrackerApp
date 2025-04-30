3a32200b358af632741cbb0019a750e886fc32c9
thuyiya <thusitha.4t@gmail.com>
2025-04-30 14:37:35 +0530

    Front page added and stlyes ui component and context

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ThemeProvider } from "@/contexts/ThemeContext";
+import Index from "./pages";
 
-function App() {
-  const [count, setCount] = useState(0)
+const queryClient = new QueryClient();
 
+const App = () => {
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+      <BrowserRouter>
+              <Routes>
+                {/* Public Routes */}
+                <Route path="/" element={<Index />} />
+              </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    </QueryClientProvider>
+  );
+};
 
-export default App
+export default App;
